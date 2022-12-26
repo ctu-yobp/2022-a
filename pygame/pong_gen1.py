25f027af70fde4aeeb62cfd057b160ab847b1f8e
jaksno <jakub.snopl@seznam.cz>
2022-12-26 12:26:13 +0100

    completion of Pong gen1
    + score
    + Pong gen2

--- pygame/pong_gen1.py
+++ pygame/pong_gen1.py
@@ -35,8 +35,8 @@ class Paddle():
 class Ball():
     def __init__(self, x, y, radius):
         pg.init()
-        self.x = x
-        self.y = y
+        self.x = self.x_or = x
+        self.y = self.y_or = y
         self.radius = radius
         self.ball_mvelocity = 5
         self.ball_color = (255,255,255)
@@ -50,10 +50,28 @@ class Ball():
         self.x += self.dx
         self.y += self.dy
 
+    def reset(self):
+        self.x = self.x_or
+        self.y = self.y_or
+        self.dx *= -1
+        self.dy = 0
+
 
 class Score():
-    pass
+    def __init__(self):
+        self.font = pg.font.SysFont("comicsans", 50)
+        self.winning_score = 2
+        self.left_score = 0
+        self.right_score = 0
 
+    # def score_counter(self):
+    #     if Pong().ball.x < 0:
+    #         Pong().score.right_score += 1
+    #         Pong().ball.reset()
+    #     elif Pong().ball.x > Pong().screen_width:
+    #         Pong().score.left_score += 1
+    #         Pong().ball.reset()
+        
 class Pong():
     def __init__(self):
         pg.init()
@@ -63,9 +81,21 @@ class Pong():
         self.fps = 60
         self.pad_height = 100
         self.pad_width = 20
+        self.white = (255,255,255)
 
-    def draw(self, screen, paddles, ball):
+        self.ball = Ball(self.screen_width//2, self.screen_height//2, 10)
+        self.left_paddle = Paddle(10, self.screen_height//2 - self.pad_height//2, self.pad_width, self.pad_height)
+        self.right_paddle = Paddle(self.screen_width - 10 - self.pad_width, self.screen_height//2 - self.pad_height//2, self.pad_width, self.pad_height)
+        self.score = Score()
+        self.screen = pg.display.set_mode((self.screen_width, self.screen_height))
+
+    def draw(self, screen, paddles, ball, left_score, right_score):
         screen.fill(self.backround_color)
+
+        left_score_text = Score().font.render(f"{left_score}", 1, self.white)
+        right_score_text = Score().font.render(f"{right_score}", 1, self.white)
+        screen.blit(left_score_text, (self.screen_width//4 - left_score_text.get_width()//2, 20))
+        screen.blit(right_score_text, (self.screen_width * 0.75 - right_score_text.get_width()//2, 20))
 
         for paddle in paddles:
             paddle.draw(screen)
@@ -100,35 +130,63 @@ class Pong():
 
                     self.bounce_angle(ball, right_paddle)
 
+   # ###########################
+   # predelat do tridy Score ???
+   # ########################### 
+    def score_counter(self):
+        if self.ball.x < 0:
+            self.score.right_score += 1
+            self.ball.reset()
+        elif self.ball.x > self.screen_width:
+            self.score.left_score += 1
+            self.ball.reset()
+
+    def end_game(self):
+        end = False
+        if self.score.left_score == self.score.winning_score:
+            end = True
+            end_text = "left player won"
+        elif self.score.right_score == self.score.winning_score:
+            end = True
+            end_text = "right player won"
+        
+        if end:
+            text = self.score.font.render(end_text, 1, self.white)
+            self.screen.blit(text, ((self.screen_width//2 - text.get_width()//2), self.screen_height//2) )
+            pg.display.update()
+            pg.time.delay(5000)
+            pg.quit()
+   # ###########################
+   # predelat do tridy Score ???
+   # ###########################
 
     def run_game(self):
-        screen = pg.display.set_mode((self.screen_width, self.screen_height))
         pg.display.set_caption("Pong")
         run = True
         clock = pg.time.Clock()
 
-        left_paddle = Paddle(10, self.screen_height//2 - self.pad_height//2, self.pad_width, self.pad_height)
-        right_paddle = Paddle(self.screen_width - 10 - self.pad_width, self.screen_height//2 - self.pad_height//2, self.pad_width, self.pad_height)
-
-        ball = Ball(self.screen_width//2, self.screen_height//2, 10)
-
         while run:
+
             clock.tick(self.fps)
-            self.draw(screen, [left_paddle, right_paddle], ball)
+
+            self.draw(self.screen, [self.left_paddle, self.right_paddle], self.ball, self.score.left_score, self.score.right_score)
 
             for event in pg.event.get():
-
                 if event.type == pg.QUIT:
                     run = False
                     break
-        
+            
             keys = pg.key.get_pressed()
-            left_paddle.paddle_bind(keys, player="left")
-            right_paddle.paddle_bind(keys, player="right")
+            self.left_paddle.paddle_bind(keys, player="left")
+            self.right_paddle.paddle_bind(keys, player="right")
 
-            ball.move()
+            self.ball.move()
 
-            self.collision(ball, left_paddle, right_paddle)
+            self.collision(self.ball, self.left_paddle, self.right_paddle)
+
+            self.score_counter()
+
+            self.end_game()
 
         pg.quit()
 
---
+++ pygame/pong_gen2.py
@@ -0,0 +1,9 @@
+import pygame as pg
+from pong_gen1 import *
+
+class Pong2(Pong):
+    pass
+
+if __name__=="__main__":
+    pong = Pong2()
+    pong.run_game()
